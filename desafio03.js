bccf3bb65efde49364617918407e1f5480f5a239
Leandro Damasco da Silva <zleo.gp@gmail.com>
2023-12-10 19:39:12 -0300

    Update desafio03.js

--- desafio03.js
+++ desafio03.js
@@ -1,4 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
 
+    atacar() {
+        let ataque = '';
 
-function
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'atacou';
+        }
 
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso da classe Heroi
+const meuHeroi = new Heroi('Guerreiro1', 25, 'mago');
+meuHeroi.atacar(); // Saída: O guerreiro atacou usando espada
